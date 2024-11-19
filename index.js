ef567b8ee394f9914e293d04592a31a4f6e10735
Autumn Lydon <autumnlydon@gmail.com>
2024-11-19 10:24:24 -0500

    pedac

---
+++ index.js
@@ -0,0 +1,14 @@
+// Problem:
+// Inputs: an integer between 1 and 2500, an array of positive and negative integers
+// Output: a string associated with the final rating of a user based off of the integer
+
+// Example: For initial = 1500 and changes = [-100, -300, 450, 500, -500, -600], the output should be solution(initial, changes) = "beginner"
+// Rating can never dip below 1 or above 2500
+
+// Data Type/ Conversion:
+//integers and manipulate it as we iterate over an array. return a string
+
+// Algorithm:
+// Create a guard clause maybe using max and min?
+// Array.for each to add each element in the array to our initial value
+// Use an if statement to determine what level the integer is equivalent to
